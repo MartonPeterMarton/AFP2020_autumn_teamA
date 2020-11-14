d04f1a9c33a9848ef6e83382d7cfae0b766b72e5
csacas <csacas7@gmail.com>
2020-11-14 15:30:46 +0100

    Create layout

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,7 +6,18 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <header>
+          <h1>AFP2020<span>_autumn_teamA</span></h1>
+      </header>
+      <nav>
+        <App />
+      </nav>
+        <footer>
+            <h4>
+                &copy; Copyright team A <br/>
+                Team: Marton Péter Márton, Dévai Levente, Bárány Gergő, Mészáros Martin
+            </h4>
+        </footer>
   </React.StrictMode>,
   document.getElementById('root')
 );
