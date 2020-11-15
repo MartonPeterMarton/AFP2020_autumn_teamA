2573c29f903418d0ea380095b18e02920f22b5b4
csacas <csacas7@gmail.com>
2020-11-15 18:38:16 +0100

    Gergő's Frontend for Azonosito

---
+++ frontend/src/component/AzonositoListItem.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function AzonositoListItem({azonosito}){
+    return (
+        <span>{azonosito} </span>
+    );
+}
+
+export default AzonositoListItem;
---
+++ frontend/src/component/AzonositoRecordingForm.js
@@ -0,0 +1,32 @@
+import React from "react";
+import * as actions from '../action/AzonositoActions';
+import ErrorMessageWell from "./ErrorMessageWell";
+class AzonositoRecordingForm extends React.Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            azonosito : 0
+        };
+        this.formOnChange = this.formOnChange.bind(this);
+    }
+
+    formOnChange(event){
+        const {name,value} = event.target;
+        this.setState({[name] : value});
+    }
+
+    render() {
+        return(
+            <div>
+                <ErrorMessageWell/>
+                <label htmlFor={"azonosito"} >Azonosito</label>
+                <input type={"number"} id={"azonosito"} name={"azonosito"} value={this.state.azonosito} onChange={this.formOnChange}/>
+                <br/>
+                <button onClick={()=> actions.recordAzonosito(this.state)}>Submit</button>
+            </div>
+        );
+    }
+}
+
+export default AzonositoRecordingForm
